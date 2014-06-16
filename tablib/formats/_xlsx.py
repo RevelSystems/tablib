7784836420c9a4d8e26b497842e421fc60502ee9
Gavin Wahl <gwahl@fusionbox.com>
2014-06-16 15:31:00 -0600

    Only freeze the headers row, not the headers columns
    Fixes #53
    
    Conflicts:
    	tablib/formats/_xlsx.py

--- tablib/formats/_xlsx.py
+++ tablib/formats/_xlsx.py
@@ -74,8 +74,7 @@ def dset_sheet(dataset, ws):
                 ws.cell('%s%s'%(col_idx, row_number)).value = unicode(col)
                 style = ws.get_style('%s%s' % (col_idx, row_number))
                 style.font.bold = True
-                ws.freeze_panes = '%s%s' % (col_idx, row_number)
-
+                ws.freeze_panes = 'A2'
 
             # bold separators
             elif len(row) < dataset.width:
